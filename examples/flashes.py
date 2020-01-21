91eec9e169e1bf1638ce6423c31ac40ee993a6a6
Christian Busse <info@christianbusse.net>
2020-01-21 15:24:30 +0100

    xoxoxo

--- examples/flashes.py
+++ examples/flashes.py
@@ -1,13 +1,13 @@
-import time
-import busio
-from board import SCL, SDA
-from adafruit_trellis import Trellis
+# import time
+# import busio
+# from board import SCL, SDA
+# from adafruit_trellis import Trellis
 
 # Create the I2C interface
-i2c = busio.I2C(SCL, SDA)
+# i2c = busio.I2C(SCL, SDA)
 
 # Create a Trellis object
-trellis = Trellis(i2c)  # 0x70 when no I2C address is supplied
+# trellis = Trellis(i2c)  # 0x70 when no I2C address is supplied
 
 # 'auto_show' defaults to 'True', so anytime LED states change,
 # the changes are automatically sent to the Trellis board. If you
